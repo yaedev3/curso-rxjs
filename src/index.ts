421a1a56b0046de0aaae1b238fb1bae4a8cd0041
GERARDO ARTURO LINDORO ENRIQUEZ CAPETILLO <gerardo.enriquez@uaslp.mx>
2023-08-31 16:33:21 -0600

    Seccion 3 video 14 terminado

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import { Observable } from 'rxjs'
 
+// const obs$ = Observable.create()
 
-console.log('Hola Mundo!');
+const obs$ = new Observable<string>((subs) => {
+  subs.next('Hola')
+  subs.next('Mundo')
 
+  subs.next('Hola')
+  subs.next('Mundo')
 
+  subs.complete()
 
+  subs.next('Hola')
+  subs.next('Mundo')
+})
 
-
-
-
+obs$.subscribe(console.log)
