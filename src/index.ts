7d73511d99a8109a0f01a091e3e4add7b62a4a47
Cathulhu mini <yaehime3@gmail.com>
2023-09-01 23:52:25 -0600

    Seccion 3 video 17 terminado

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,29 @@ const observer: Observer<any> = {
   error: (error) => console.warn('error [obs]', error),
   complete: () => console.info('completado'),
 }
+
+const intervalo$ = new Observable<number>((subscriber) => {
+  let count = 0
+  const interval = setInterval(() => {
+    count++
+    subscriber.next(count)
+    console.log(count)
+  }, 1000)
+
+  return () => {
+    clearInterval(interval)
+    console.log('Intervalo destruido')
+  }
+})
+
+const subs1 = intervalo$.subscribe()
+const subs2 = intervalo$.subscribe()
+const subs3 = intervalo$.subscribe()
+
+setTimeout(() => {
+  subs1.unsubscribe()
+  subs2.unsubscribe()
+  subs3.unsubscribe()
+
+  console.log('Completado timeout')
+}, 3000)
