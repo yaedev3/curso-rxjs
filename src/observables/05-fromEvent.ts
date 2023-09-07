dc92a58e0e0d437a97684bcefe1a2f779f52ef32
Cathulhu mini <yaehime3@gmail.com>
2023-09-06 23:49:54 -0600

    Seccion 4 video 25 terminado

---
+++ src/observables/05-fromEvent.ts
@@ -0,0 +1,16 @@
+import { fromEvent } from 'rxjs'
+
+const src1$ = fromEvent<MouseEvent>(document, 'click')
+const src2$ = fromEvent<KeyboardEvent>(document, 'keyup')
+
+const observer = {
+  next: (val) => console.log('next', val),
+}
+
+src1$.subscribe(({ x, y }) => {
+  console.log(x)
+  console.log(y)
+})
+src2$.subscribe((event) => {
+  console.log(event.key)
+})
