6ccc4cf0e850787d70cb94239dd32c3fdb2d8f1c
Cathulhu mini <yaehime3@gmail.com>
2023-09-08 23:31:19 -0600

    Seccion 4 video 27 terminado

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import { asyncScheduler, of, range } from 'rxjs'
+import { interval, timer } from 'rxjs'
 
-// const src$ = of(1,2,3,4,5)
-const src$ = range(1, 5, asyncScheduler)
+const observer = {
+  next: (val) => console.log('next', val),
+  complete: () => console.log('complete'),
+}
+
+const interval$ = interval(1000)
+const timer$ = timer(2000)
 
 console.log('inicio')
-src$.subscribe(console.log)
+// interval$.subscribe(observer)
+timer$.subscribe(observer)
 console.log('fin')
