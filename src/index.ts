3f5da1ca831f1e5e26d267db15e1ecf1e8c037bf
Cathulhu mini <yaehime3@gmail.com>
2023-09-14 23:58:07 -0600

    Seccion 5 video 36 terminado

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { fromEvent, map, range } from 'rxjs'
+import { fromEvent, map, pluck, range } from 'rxjs'
 
 // range(1, 5)
 //   .pipe(map<number, string>((value) => (value * 10).toString()))
@@ -8,4 +8,8 @@ const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup')
 
 const keyupCode$ = keyup$.pipe(map((event) => event.code))
 
+const keyupPluck$ = keyup$.pipe(pluck('target', 'baseURI'))
+
+keyup$.subscribe(console.log)
 keyupCode$.subscribe((value) => console.log('map', value))
+keyupPluck$.subscribe((value) => console.log('pluck', value))
