f6eee1c4ae85683ef7e7bc284e7d16ff5cbea4b7
Cathulhu mini <yaehime3@gmail.com>
2023-10-03 23:44:35 -0600

    Seccion 7 video 60 terminado

---
+++ src/operadores-tiempo/02-throttleTime.ts
@@ -0,0 +1,29 @@
+import {
+  asyncScheduler,
+  debounceTime,
+  distinctUntilChanged,
+  fromEvent,
+  pluck,
+  throttleTime,
+} from 'rxjs'
+
+const click$ = fromEvent(document, 'click')
+
+click$.pipe(throttleTime(3000))
+// .subscribe(console.log)
+
+// Ejemplo 2
+const input = document.createElement('input')
+document.querySelector('body').append(input)
+
+const input$ = fromEvent(input, 'keyup')
+input$
+  .pipe(
+    throttleTime(400, asyncScheduler, {
+      leading: false,
+      trailing: true,
+    }),
+    pluck('target', 'value'),
+    distinctUntilChanged()
+  )
+  .subscribe(console.log)
