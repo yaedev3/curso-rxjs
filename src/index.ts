e28f2120ca003e4b95dfc03634c05194767b7872
GERARDO ARTURO LINDORO ENRIQUEZ CAPETILLO <gerardo.enriquez@uaslp.mx>
2023-10-05 15:45:06 -0600

    fix: Error al purgar el index

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { fromEvent, interval, sample } from 'rxjs'
-
-const interval$ = interval(500)
-const click$ = fromEvent(document, 'click')
-
-interval$.pipe(sample(click$)).subscribe(console.log)
