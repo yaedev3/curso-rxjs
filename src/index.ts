e73cbbb2feb65cfba7c4c7330ee60dbc1080c361
GERARDO ARTURO LINDORO ENRIQUEZ CAPETILLO <gerardo.enriquez@uaslp.mx>
2023-10-05 15:44:34 -0600

    Seccion 7 video 62 terminado

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { fromEvent, interval, sample } from 'rxjs'
+
+const interval$ = interval(500)
+const click$ = fromEvent(document, 'click')
+
+interval$.pipe(sample(click$)).subscribe(console.log)
