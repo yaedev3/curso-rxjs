afa972e6c36590b538ed28b2e7acfaf06f70bdf1
GERARDO ARTURO LINDORO ENRIQUEZ CAPETILLO <gerardo.enriquez@uaslp.mx>
2023-10-05 15:48:27 -0600

    Seccion 7 video 63 terminado

---
+++ src/operadores-tiempo/05-auditTime.ts
@@ -0,0 +1,11 @@
+import { auditTime, fromEvent, map, tap } from 'rxjs'
+
+const click$ = fromEvent<MouseEvent>(document, 'click')
+
+click$
+  .pipe(
+    map(({ x }) => x),
+    tap((val) => console.log('tap', val)),
+    auditTime(2000)
+  )
+  .subscribe(console.log)
