9cebd85972668cb8b88de58fefb2e583518e467b
GERARDO ARTURO LINDORO ENRIQUEZ CAPETILLO <gerardo.enriquez@uaslp.mx>
2023-10-19 16:48:56 -0600

    Seccion 9 video 84 terminado

---
+++ src/transformacion/06-exhaustMap.ts
@@ -0,0 +1,6 @@
+import { exhaustMap, fromEvent, interval, take } from 'rxjs'
+
+const interval$ = interval(500).pipe(take(3))
+const click$ = fromEvent(document, 'click')
+
+click$.pipe(exhaustMap(() => interval$)).subscribe(console.log)
